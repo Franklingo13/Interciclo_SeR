8b2a510e8a2c47ae5a572a4c066f4e829b0e9a5a
Franklin <franklin.gomez@ucuenca.edu.ec>
2022-11-14 17:53:24 -0500

    Se crea el código C ejemplo para imprimir el contenido de una matriz

---
+++ LabC6/Labsetup/matriz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+unsigned char xyz[200] = {
+/* The actual contents of this array are up to you */
+};
+int main()
+{
+int i;
+for (i=0; i<200; i++){
+printf("%x", xyz[i]);
+}
+printf("\n");
+}
