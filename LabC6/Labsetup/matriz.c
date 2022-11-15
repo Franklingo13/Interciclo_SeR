a605bab5e5a8b951da7591bcbf5e023aba4d9c63
FranklinSeed <franklin.gomez@ucuenca.edu.ec>
2022-11-15 12:02:43 -0500

    Se rellena la matriz con 200 letras F

--- LabC6/Labsetup/matriz.c
+++ LabC6/Labsetup/matriz.c
@@ -3,8 +3,9 @@ unsigned char xyz[200] = {"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 int main()
 {
 int i;
-for (i=0; i<200; i++){
-printf("%x", xyz[i]);
-}
-printf("\n");
+	for (i=0; i<200; i++){
+	printf("%x", xyz[i]);
+	}
+	printf("\n");
+	
 }
